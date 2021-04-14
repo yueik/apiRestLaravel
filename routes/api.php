cb63d7c42e7774d95236ddd47f8851eea62fc360
JoaquinLinux <niujoacog5@gmail.com>
2021-04-14 16:20:05 -0700

    Configuración Inicial de Versión 2

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
-Route::apiResource('v1/posts', \App\Http\Controllers\Api\V1\PostController::class)
+//Version 1
+Route::apiResource('v1/posts', PostV1::class)
+    ->only(['index', 'show', 'destroy']);
+
+//Version 2
+Route::apiResource('v2/posts', PostV2::class)
     ->only(['index', 'show', 'destroy']);
