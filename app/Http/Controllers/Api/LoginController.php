935d19e26bb73f5e150b442d663ad5d9fb0d9147
JoaquinLinux <niujoacog5@gmail.com>
2021-04-14 18:05:24 -0700

    Acceso y Autenticación

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login(Request $request) {
+        $this->validateLogin($request);
+
+        if(Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'token' =>$request->user()->createToken($request->name)->plainTextToken
+            ]);
+        }else{
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+    }
+
+    public function validateLogin(Request $request) {
+        return $request->valisate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'name' => 'required'
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,16 @@ use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 //Version 1
 Route::apiResource('v1/posts', PostV1::class)
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
 
 //Version 2
 Route::apiResource('v2/posts', PostV2::class)
-    ->only(['index', 'show', 'destroy']);
+    ->only(['index', 'show', 'destroy'])
+    ->middleware('auth:sanctum');
+
+//Login
+Route::post('login', [
+    App\Http\Controllers\Api\LoginController::class,
+    'login'
+]);
